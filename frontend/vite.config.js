f1fc9a979fb85ea6821b160ebff65f21dd401c0d
Nathan Cloutier <nathanc4571@gmail.com>
2025-06-22 14:17:31 -0400

    Use vite-plugin-pwa (not tested)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({
+    registerType: 'autoUpdate',
+    workbox: {
+      runtimeChaching: [
+        {
+          urlPattern: /^\/downloads\/.*$/,
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'downloads-pages',
+            expiration: {
+              maxEntries: 30,
+            },
+            fallbackURL: '/downloads',
+          },
+        },
+      ],
+    },
+    manifest: {
+      name: 'Flash Forge',
+      short_name: 'Flash Forge',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#4B5563',
+    }
+  })],
 })
