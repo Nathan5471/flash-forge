14732b829aa9d98f9531cb3ff2c7c0be372805b8
Nathan Cloutier <nathanc4571@gmail.com>
2025-06-25 22:30:30 -0400

    Update offline learn manager to have response equivalent to api

--- frontend/src/utils/OfflineLearnManager.js
+++ frontend/src/utils/OfflineLearnManager.js
@@ -1,33 +1,47 @@
-import { getDownloadedFlashcardSet } from "./DownloadManager";
+const internalGetFlashcardSet = (flashcardSetId) => {
+    const flashcardSetData = localStorage.getItem(`flashcardSet-${flashcardSetId}`);
+    if (!flashcardSetData) {
+        return null;
+    }
+    return JSON.parse(flashcardSetData);
+}
 
-export const checkIfLearnSessionExists = (flashcardSetId) => {
+const internalGetLearnSession = (id) => {
+    const learnSessionData = localStorage.getItem(id);
+    if (!learnSessionData) {
+        return null;
+    }
+    return JSON.parse(learnSessionData);
+}
+
+export const checkIfLearnSessionExists = async (flashcardSetId) => {
     try {
-        const flashcardSet = getDownloadedFlashcardSet(flashcardSetId);
+        const flashcardSet = JSON.parse(localStorage.getItem(`flashcardSet-${flashcardSetId}`));
         if (!flashcardSet) {
-            throw new Error('Flashcard set not found');
+            return Promise.reject({ message: 'Flashcard set not found'})
         }
         const learnSession = JSON.parse(localStorage.getItem(`learn-${flashcardSetId}`));
         if (!learnSession) {
-            return false;
+            return { learnSessionId: null };
         }
-        return learnSession._id;
+        return { learnSessionId: learnSession._id };
     } catch (error) {
         console.error('Error checking learn session:', error);
-        return false;
+        return Promise.reject({ message: 'Internal server error' });
     }
 }
 
-export const createLearnSession = (flashcardSetId, settings) => {
+export const createLearnSession = async (flashcardSetId, settings) => {
     try {
         if (!flashcardSetId || !settings) {
-            throw new Error('Flashcard set ID and settings are required');
+            return Promise.reject({ message: 'Flashcard set ID and settings are required' });
         }
-        const flashcardSet = getDownloadedFlashcardSet(flashcardSetId);
+        const flashcardSet = internalGetFlashcardSet(flashcardSetId);
         if (!flashcardSet) {
-            throw new Error('Flashcard set not found');
+            return Promise.reject({ message: 'Flashcard set not found' });
         }
-        if (checkIfLearnSessionExists(flashcardSetId)) {
-            throw new Error('Learn session already exists for this flashcard set');
+        if (await checkIfLearnSessionExists(flashcardSetId)) {
+            return Promise.reject({ message: 'Learn session already exists for this flashcard set' });
         }
         let questions = [];
         let trueFalseAmount = settings.trueFalseAmount;
@@ -76,62 +90,59 @@ export const createLearnSession = (flashcardSetId, settings) => {
             createdAt: new Date()
         }
         localStorage.setItem(`learn-${flashcardSetId}`, JSON.stringify(learnSession));
-        return learnSession;
+        return { learnSessionId: learnSession._id };
     } catch (error) {
         console.error('Error creating learn session:', error);
-        return null;
+        return Promise.reject({ message: 'Internal server error' });
     }
 }
 
-export const getLearnSession = (id) => {
+export const getLearnSession = async (id) => {
     try {
         if (!id) {
-            throw new Error('ID is required');
+            return Promise.reject({ message: 'ID is required' });
         }
         const learnSession = localStorage.getItem(id);
         if (!learnSession) {
-            throw new Error('Learn session not found');
+            return Promise.reject({ message: 'Learn session not found' });
         }
         return JSON.parse(learnSession);
     } catch (error) {
         console.error('Error retrieving learn session:', error);
-        return null;
+        return Promise.reject({ message: 'Interal server error' });
     }
 }
 
-export const deleteLearnSession = (id) => {
+export const deleteLearnSession = async (id) => {
     try {
         if (!id) {
-            throw new Error('ID is required');
+            return Promise.reject({ message: 'ID is required' });
         }
         localStorage.removeItem(id);
         return true;
     }  catch (error) {
         console.error('Error deleting learn session:', error);
-        return false;
+        return Promise.reject({ message: 'Internal server error' });
     }
 }
 
-export const generateLearnSession = (id) => {
+export const generateLearnSession = async (id) => {
     try {
         if (!id) {
-            throw new Error('ID is required');
+            return Promise.reject({ message: 'ID is required' });
         }
-        const learnSession = getLearnSession(id);
+        const learnSession = internalGetLearnSession(id);
         if (!learnSession) {
-            throw new Error('Learn session not found for this flashcard set');
+            return Promise.reject({ message: 'Learn session not found' });
         }
         const settings = learnSession.settings;
-        const flashcardSet = getDownloadedFlashcardSet(learnSession.flashcardSet);
+        const flashcardSet = internalGetFlashcardSet(learnSession.flashcardSet);
         if (!flashcardSet) {
-            throw new Error('Flashcard set not found');
+            return Promise.reject({ message: 'Flashcard set not found' });
         }
         const questions = [...learnSession.questions].sort((a, b) => a.order - b.order);
         const populatedQuestions = questions.splice(0, settings.amountPerSession).map(question => {
             const flashcard = flashcardSet.flashCards.find(flashcard => flashcard._id === question.flashcard);
-            if (!flashcard) {
-                throw new Error('Flashcard not found in flashcard set');
-            }
             return {
                 ...question,
                 flashcard: flashcard
@@ -140,39 +151,39 @@ export const generateLearnSession = (id) => {
         return { questions: populatedQuestions };
     } catch (error) {
         console.error('Error generating learn session:', error);
-        return null;
+        return Promise.reject({ message: 'Internal server error' });
     }
 }
 
 export const checkAnswer = (id, order, answer) => {
     try {
         if (!id || order === undefined || !answer) {
-            throw new Error('ID, order, and answer are required');
+            return Promise.reject({ message: 'ID, order, and answer are required' });
         }
-        const learnSession = getLearnSession(id);
+        const learnSession = internalGetLearnSession(id);
         if (!learnSession) {
-            throw new Error('Learn session not found');
+            return Promise.reject({ message: 'Learn session not found' });
         }
         const question = learnSession.questions.find(question => question.order === order);
         if (!question) {
-            throw new Error('Question not found in learn session');
+            return Promise.reject({ message: 'Question not found in learn session' });
         }
-        const flashcardSet = getDownloadedFlashcardSet(learnSession.flashcardSet);
+        const flashcardSet = internalGetFlashcardSet(learnSession.flashcardSet);
         if (!flashcardSet) {
-            throw new Error('Flashcard set not found');
+            return Promise.reject({ message: 'Flashcard set not found' });
         }
         const flashcard = flashcardSet.flashCards.find(flashcard => flashcard._id === question.flashcard);
         if (!flashcard) {
-            throw new Error('Flashcard not found in flashcard set');
+            return Promise.reject({ message: 'Flashcard not found' });
         }
         const isCorrect = flashcard.answer.toLowerCase() === answer.toLowerCase()
         if (isCorrect) {
             learnSession.questions = learnSession.questions.filter(question => question.order.toString() !== order);
             localStorage.setItem(`learn-${id}`, JSON.stringify(learnSession));
         }
-        return { isCorrect, flashcard };
+        return { isCorrect, correctAnswer: flashcard.answer };
     } catch (error) {
         console.error('Error checking answer:', error);
-        throw error;
+        return Promise.reject({ message: 'Internal server error' });
     }
 }
