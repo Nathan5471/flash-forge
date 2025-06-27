a637c696ddd55d15c1f37be0d26af10b75985707
Nathan Cloutier <nathanc4571@gmail.com>
2025-06-27 03:54:35 -0400

    Start redesign and improve mobile experience (login and register)

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -23,45 +23,45 @@ export default function Login() {
         navigate('/')
     }
     return (
-        <div className="flex items-center justify-center w-screen h-screen bg-gray-700">
-            <div className="bg-gray-600 p-6 rounded-lg text-white w-[calc(30%)]">
-                <h1 className="text-2xl mb-4">Login</h1>
-                {error && <p className="text-red-500 mb-2">{error}</p>}
+        <div className="flex items-center justify-center w-screen h-screen bg-[#251d24] text-white">
+            <div className="bg-[#282828] p-6 rounded-lg w-[calc(80%)] sm:w-[calc(65%)] md:w-1/2 lg:w-[calc(30%)]">
+                <h1 className="text-4xl text-[#f081e7] font-bold text-center mb-4">Login</h1>
                 <form>
                     <div className="mb-2">
-                        <label className="block mb-1" htmlFor="username">Email</label>
+                        <label className="text-xl block mb-1" htmlFor="username">Email</label>
                         <input
                             type="text"
                             id="email"
                             name="email"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
-                            className="w-full p-2 rounded bg-gray-500 text-white"
+                            className="w-full p-2 rounded bg-[#3f3f3f]"
                             placeholder="Enter your email"
                         />
                     </div>
-                    <div className="mb-2">
-                        <label className="block mb-1" htmlFor="password">Password</label>
+                    <div className="mb-4">
+                        <label className="text-xl block mb-1" htmlFor="password">Password</label>
                         <input
                             type="password"
                             id="password"
                             name="password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
-                            className="w-full p-2 rounded bg-gray-500 text-white"
+                            className="w-full p-2 rounded bg-[#3f3f3f]"
                             placeholder="Enter your password"
                         />
                     </div>
+                    {error && <p className="text-red-500 mb-4">{error}</p>}
                     <button
                         type="submit"
                         onClick={handleSubmit}
-                        className="w-full bg-blue-500 hover:bg-blue-600 text-white p-2 rounded"
+                        className="w-full bg-[#f081e7] hover:bg-[#f390ea] p-2 rounded"
                     >
                         Login
                     </button>
                 </form>
-                <p className="mt-4 text-sm">
-                    Don't have an account? <a href="/register" className="text-blue-400 hover:underline">Register</a>
+                <p className="mt-4 text-pretty">
+                    Don't have an account? <a href="/register" className="text-[#f081e7] hover:underline">Register</a>
                 </p>
             </div>
         </div>
