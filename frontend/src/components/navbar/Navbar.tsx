286a168af8c52fbb674e5436b17009c90f564d1d
nathan5471 <nathanc4571@gmail.com>
2026-07-31 13:14:04 -0400

    Add more links to navbar

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,12 +1,32 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 import styles from "./Navbar.module.css";
 
 function Navbar() {
+  const { user } = useAuth();
+
   return (
     <div className={styles.navbarContainer}>
       <Link to="/" className={styles.navbarLogo}>
         Flash Forge
       </Link>
+      <div className={styles.navbarContainerRight}>
+        {user === null && (
+          <Link to="/login" className={styles.navbarLink}>
+            Login
+          </Link>
+        )}
+        {user && (
+          <Link to={`/user/${user.username}`} className={styles.navbarLink}>
+            My Sets
+          </Link>
+        )}
+        {user && (
+          <Link to="/create" className={styles.navbarLink}>
+            Create
+          </Link>
+        )}
+      </div>
     </div>
   );
 }
